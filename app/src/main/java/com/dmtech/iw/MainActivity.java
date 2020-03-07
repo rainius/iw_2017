6289dcf09df0523f9bea6e13fb978f62780412c7
Zhang Jing <jingz.bit@gmail.com>
2020-03-07 14:12:41 +0800

    Add Toolbar

--- app/src/main/java/com/dmtech/iw/MainActivity.java
+++ app/src/main/java/com/dmtech/iw/MainActivity.java
@@ -1,14 +1,73 @@
 package com.dmtech.iw;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_drawer_layout);
+
+        // 用Toolbar代替Actionbar
+        mToolbar = findViewById(R.id.main_toolbar);
+        this.setSupportActionBar(mToolbar);
+
+        mDrawer = findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawer, mToolbar, R.string.open, R.string.close);
+
+        mDrawer.addDrawerListener(mDrawerToggle);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //依据菜单资源文件描述创建菜单对象
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // 使抽屉按钮状态与抽屉视图的开闭状态一致
+        mDrawerToggle.syncState();
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else if (item.getItemId() == R.id.menu_item_add) {
+            Toast.makeText(this, "增加新位置", Toast.LENGTH_SHORT).show();
+            Log.d("iWeather", "增加新位置");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
